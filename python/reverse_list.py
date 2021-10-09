346a0a7a78863fd856a569ec524dad16a7dedb69
Ranvijay Sachan <sachan.ranvijay@gmail.com>
2021-10-09 11:46:23 +0530

    Update reverse_list.py

--- python/reverse_list.py
+++ python/reverse_list.py
@@ -1,3 +1,4 @@
+# using slices
 a = [1, 2, 3, 4]
 
 print a[::-1]
@@ -6,3 +7,11 @@ print a[::-1]
 
 a.reverse()
 print a
+
+#using manaul
+
+x = [3, 4, 100, 34, 45]
+for i in range(len(x) - 1):
+    if x[i] > x[i + 1]:
+        x[i],x[i + 1] = x[i + 1], x[i]
+print (x)
