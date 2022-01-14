98e88ac3bdb921915545e30f578535f3445e21d7
Ranvijay Sachan <sachan.ranvijay@gmail.com>
2022-01-14 12:55:53 +0530

    Update palindrome_number.py

--- python/palindrome_number.py
+++ python/palindrome_number.py
@@ -13,3 +13,24 @@ def is_palindrome(text, ignoreCase=True):
 
 if __name__ == '__main__':
     print is_palindrome('rar')
+    
+
+    
+# palindrome by removing one character
+
+Python easy to understand O(n) time O(1) space solution.
+
+    def isPalindrome(self, start, end, s, deletionAllowed):
+        while start <= end:
+            if s[start] == s[end]:
+                start += 1
+                end -= 1
+            elif deletionAllowed:
+                return self.isPalindrome(start+1, end, s, False) or self.isPalindrome(start, end-1, s, False)
+            else:
+                return False
+        return True
+    
+    def validPalindrome(self, s: str) -> bool:
+        deletionAllowed = True
+        return self.isPalindrome(0, len(s)-1, s, deletionAllowed)
