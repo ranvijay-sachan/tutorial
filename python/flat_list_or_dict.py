9d72b23769f37e3904724814cfcf215fca774e0c
Ranvijay Sachan <sachan.ranvijay@gmail.com>
2022-01-15 12:11:04 +0530

    Update flat_list_or_dict.py

--- python/flat_list_or_dict.py
+++ python/flat_list_or_dict.py
@@ -11,6 +11,20 @@ daList = ['1', 4, [1, 2], [5, 6], [1, 'e'], {"c": 2}]
 print(flatten(daList))
 
 
+def flatten(unflattened_dict, separator='_'):
+    flattened_dict = {}
+
+    for k, v in unflattened_dict.items():
+        if isinstance(v, dict):
+            sub_flattened_dict = flatten(v, separator)
+            for k2, v2 in sub_flattened_dict.items():
+                flattened_dict[k + separator + k2] = v2
+        else:
+            flattened_dict[k] = v
+
+    return flattened_dict
+
+
 def flat_items(d, key_separator='.'):
     """
     Flattens the dictionary containing other dictionaries like here: https://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
