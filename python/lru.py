d29582b77ea5ff53df7ef6797e88335d3f2f57bc
Ranvijay S <sachan.ranvijay@gmail.com>
2024-11-08 16:52:29 +0530

    Update lru.py

--- python/lru.py
+++ python/lru.py
@@ -1,29 +1,68 @@
-import collections
+class Node:
+    """A doubly linked list node to store key-value pairs."""
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
-class Lru(objects):
-   def __init__(self):
-       self.capacity = 5
-       self.buffer = collection.Ordereddict()
-       self.current_size = 0
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # Stores key to node mapping
+        self.head = Node(0, 0)  # Dummy head node
+        self.tail = Node(0, 0)  # Dummy tail node
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
+    def _remove(self, node: Node):
+        """Remove an existing node from the doubly linked list."""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
-   def get(self, key):
-       if key in self.buffer:
-          return self.buffer[key]
-       else:
-          return "Not found"
-       
+    def _add(self, node: Node):
+        """Add a node right after the head (most recently used)."""
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
 
-   def insert(self, key, value):
-       if key in self.buffer:
-          return "already exist"
-       elif self.current_size < self.capacity:
-          self.buffer[key] = value
-          self.current_size += 1
+    def get(self, key: int) -> int:
+        """Get the value of the key if the key exists, otherwise return -1."""
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)  # Move the accessed node to the front
+            return node.value
+        return -1
 
-       elif self.current_size >= self.capacity:
-            self.buffer.pop()
-            self.buffer[key] = value
+    def put(self, key: int, value: int) -> None:
+        """Insert or update the value of the key."""
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+        elif len(self.cache) >= self.capacity:
+            # Remove the LRU from the cache and the list
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+        
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self._add(new_node)
+
+# Example usage:
+lru = LRUCache(3)
+lru.put(1, 1)
+lru.put(2, 2)
+lru.put(3, 3)
+print(lru.get(1))  # Returns 1
+lru.put(4, 4)  # Evicts key 2
+print(lru.get(2))  # Returns -1 (not found)
+print(lru.get(3))  # Returns 3
+print(lru.get(4))  # Returns 4
 
 
 
